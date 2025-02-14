483392c9b0cb70d927efdd9cf38aaa27de076a5e
Manuel Chacana <mchacana@whiteboard.cl>
2025-02-14 14:50:56 -0300

    se agrega el filtrado de categorias

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,11 @@ class MoviesController < ApplicationController
   
   def index
     @categories = Category.order(name: :asc).load_async
-    @movies = Movie.all.order(created_at: :desc).load_async
+    @movies = Movie.all.includes(:categories)
+    if product_params_index[:category_id].present?
+      @movies = @movies.where("category_movies.category_id"  => product_params_index[:category_id])
+    end
+    @movies = @movies.order('movies.release_date asc').load_async
   end
 
   def new
@@ -33,5 +37,9 @@ class MoviesController < ApplicationController
   private
   def movie_params
     params.require(:movie).permit(:title, :original_title, :overview, :release_date, :poster_path, :original_language, category_ids: [])
+  end
+
+  def product_params_index
+    params.permit(:category_id)
   end
 end
