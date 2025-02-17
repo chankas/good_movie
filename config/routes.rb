10b627054a05c2391b00dea3f6b0882db1fa9c66
Manuel Chacana <mchacana@whiteboard.cl>
2025-02-16 23:01:20 -0300

    Se crea modelo usuario

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
   resources :movies
   resources :categories
+
+  namespace :authentications, path: '', as: '' do
+    resources :users, only: %i[new create]
+  end
 end
