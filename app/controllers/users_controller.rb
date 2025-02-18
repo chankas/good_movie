cf8163953d816bee87dd6b8e28d59d2d69061d07
Manuel Chacana <mchacana@whiteboard.cl>
2025-02-18 17:57:01 -0300

    Refactor se simplifica lista de peliculas del usuario usando turbo_frame

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,5 @@ class UsersController < ApplicationController
   skip_before_action :protect_pages
   def show
     @user = User.find_by!(username: params[:username])
-    @categories = Category.order(name: :asc).load_async
-    @pagy, @movies = pagy_countless( FindMovies.new.call({user_id: @user.id}).load_async,  items: 9)
   end
 end
