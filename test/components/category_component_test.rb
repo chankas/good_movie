9553d279d9c69e3644a230d6e82f45b117378871
Manuel Chacana <mchacana@whiteboard.cl>
2025-02-21 13:09:44 -0300

    se cambian los nombres a los test para hacerlos mas reprecentativos

--- test/components/category_component_test.rb
+++ test/components/category_component_test.rb
@@ -3,14 +3,14 @@
 require "test_helper"
 
 class CategoryComponentTest < ViewComponent::TestCase
-  test "renders a category all" do
+  test "render a category component with a nil category" do
     assert_equal(
     %(<a class=\"category bg-gray-300 text-gray-600 px-4 py-2 rounded-2xl whitespace-nowrap drop-shadow-sm hover:bg-gray-300 \" href=\"/\">Todas</a>),
       render_inline(CategoryComponent.new).to_html
     )
   end
 
-  test "renders a category with a name" do
+  test "render a category compoment whit a category" do
     @category = categories(:accion)
     assert_equal(
     %(<a class=\"category bg-white text-gray-600 px-4 py-2 rounded-2xl whitespace-nowrap drop-shadow-sm hover:bg-gray-300 \" href=\"/\?category_id=#{@category.id}\">#{@category.name}</a>),
